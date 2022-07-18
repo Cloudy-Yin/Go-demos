a0beb0fbaffd8223703d0ae8761099c371e1b732
yinhuile <1963242128@qq.com>
2022-07-18 10:37:12 +0800

    Add slice_demo;

---
+++ slice_demo/go.mod
@@ -0,0 +1,3 @@
+module slice_demo
+
+go 1.18
---
+++ slice_demo/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 []int
+	var num2 = []int{1, 2, 3}
+	num3 := append(num1, num2...)
+	fmt.Println(len(num3), num3)
+
+	var num4 []interface{}
+	num5 := []int{1, 2, 3}
+	num6 := append(num4, num5)
+	num6 = append(num6, 1, 2, 3, 4, 5)
+	fmt.Println(len(num6), num6)
+
+}
